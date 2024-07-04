193aeebb2cd2b764e5c7e4270d71d52f9420b0ed
PHyo Sithu Kyaw <phyosithukyaw2002@gmail.com>
2024-07-04 15:50:43 +0630

    Added stack layout

---
+++ app/_layout.jsx
@@ -0,0 +1,11 @@
+import { Stack } from "expo-router";
+
+export default function App() {
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="detail" options={{ headerShown: false }} />
+      <Stack.Screen name="character" options={{ headerShown: false }} />
+    </Stack>
+  );
+}
